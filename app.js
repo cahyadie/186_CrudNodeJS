c0f13727ba1081623336ac98addb3889055e6c7f
cahyadie <cahyadiilham12@gmail.com>
2024-10-23 19:23:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(port, () => {
+  console.log("Server running at http://localhost:${5000}/");
+});
