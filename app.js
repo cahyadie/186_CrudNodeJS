9ae69f337b2c448f9160a0ab437808b1f26b0571
cahyadie <cahyadiilham12@gmail.com>
2024-10-28 10:20:22 +0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const todoRoutes = require('./routes/todos.js');
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
@@ -11,6 +15,11 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.listen(port, () => {
-  console.log("Server running at http://localhost:${5000}/");
+app.use((req, res) => {
+  res.status(404).send('404 - Page Not Found');
 });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
